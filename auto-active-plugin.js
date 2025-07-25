3d7cf72382f66c614569fd60df6736bb8540e014
Dawn Hiu <113577868+HieuDang2000@users.noreply.github.com>
2025-07-25 21:31:41 +0700

    update

--- auto-active-plugin.js
+++ auto-active-plugin.js
@@ -3,13 +3,14 @@ const step2 = document.querySelector("#toplevel_page_wpscore-dashboard > a > div
 const step3_1 = document.querySelector("#dashboard > div.tab-content > div > div > div.v-cloak--hidden > div > div > div > div > div > div.row > div > div > input")
 const step3_2 = document.querySelector("#dashboard > div.tab-content > div > div > div.v-cloak--hidden > div > div > div > div > div > div.row > div > div > span.input-group-btn > button")
 const step4 = document.querySelector("#toplevel_page_wpscore-dashboard > ul > li:nth-child(4) > a");
+const fullurl = window.location.href;
 
 if (step1) {
     simulateClick(step1);
     console.log("step1");
 }
 else {
-    if (step2 && !step4) {
+    if (step2 && fullurl.includes("wp-admin/plugins.php?plugin_status")) {
         simulateClick(step2);
         console.log("step2");
     }
@@ -42,31 +43,18 @@ function simulateClick(element) {
     element.dispatchEvent(new MouseEvent('click', {bubbles: true}));
 }
 
-// Mô phỏng gõ phím từng ký tự
+// Mô phỏng gõ phím đơn giản hơn
 function simulateTyping(element, text) {
     // Focus vào input
     element.dispatchEvent(new Event('focus', {bubbles: true}));
     
-    // Xóa giá trị hiện tại
-    element.value = '';
+    // Đặt giá trị và kích hoạt sự kiện input
+    element.value = text;
     element.dispatchEvent(new Event('input', {bubbles: true}));
     
-    let index = 0;
+    // Kích hoạt sự kiện change
+    element.dispatchEvent(new Event('change', {bubbles: true}));
     
-    // Gõ từng ký tự một với độ trễ
-    function typeNextChar() {
-        if (index < text.length) {
-            const char = text.charAt(index);
-            element.value += char;
-            element.dispatchEvent(new Event('input', {bubbles: true}));
-            index++;
-            setTimeout(typeNextChar, Math.random() * 100 + 50); // Độ trễ ngẫu nhiên giữa 50-150ms
-        } else {
-            // Hoàn thành việc gõ
-            element.dispatchEvent(new Event('change', {bubbles: true}));
-        }
-    }
-    
-    typeNextChar();
-    return new Promise(resolve => setTimeout(resolve, text.length * 150));
+    // Trả về promise đã giải quyết sau một khoảng thời gian ngắn
+    return Promise.resolve(setTimeout(() => {}, 100));
 }
